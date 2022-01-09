540de139baffd11011437afd7b55738098277718
A-S620 <aatikaseedat00@gmail.com>
2022-01-09 10:48:35 +0000

    completed quote-api

--- quote-api/server.js
+++ quote-api/server.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const {quotes} = require('./data');
+const {getRandomElement} = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({quote: randomQuote})
+
+})
+app.get('/api/quotes/', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        res.send({quotes: quotes.filter(quote => quote.person === person)})
+    } else {
+        res.send({quotes: quotes})
+    }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const quote = req.query.quote;
+    const person = req.query.person;
+    if (quote && person) {
+        let quoteToAdd = {quote: quote, person: person};
+        quotes.push(quoteToAdd)
+        res.send({quote: quoteToAdd})
+    } else {
+        res.status(400).send()
+    }
+})
