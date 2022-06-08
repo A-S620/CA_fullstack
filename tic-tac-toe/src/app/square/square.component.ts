2cdf4289fffe57b975202bd71ab4781bbd7efd50
A-S620 <aatikaseedat00@gmail.com>
2022-06-08 20:47:50 +0100

    setup theme

--- tic-tac-toe/src/app/square/square.component.ts
+++ tic-tac-toe/src/app/square/square.component.ts
@@ -3,16 +3,12 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <div class="square">
-      <h1>
-        {{value}}
-      </h1>
-      </div>
+    <button nbButton *ngIf="!value" >{{value}}</button>
+    <button nbButton hero *ngIf="value == 'X'" status="success" >{{value}}</button>
+    <button nbButton hero *ngIf="value == 'O'" status="info" >{{value}}</button>
+
   `,
-  styles: [
-    `
-    `
-  ]
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; color: purple }']
 })
 export class SquareComponent {
   @Input() value!: 'X' | 'O';
